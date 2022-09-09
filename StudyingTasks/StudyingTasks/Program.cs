34ed4b2ce26189972b01922d3ca92f7dd42a21d0
AlinaBelahrudava <alina.belahrudava@gmail.com>
2022-09-09 15:31:51 +0300

    ready solution

--- StudyingTasks/StudyingTasks/Program.cs
+++ StudyingTasks/StudyingTasks/Program.cs
@@ -1,1 +1,45 @@
-﻿// First task will be here
+﻿using System;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Please enter your string and I will find the longest sequence of unique ones");
+        string? Line = Console.ReadLine();
+        string Sequence = String.Empty;
+
+        if (!String.IsNullOrEmpty(Line))
+        {
+            string Subsequence = String.Empty;
+            for (int i = 0; i < Line.Length; i++)
+            {
+                if (!Subsequence.Contains(Line[i]))
+                {
+                    Subsequence += Line[i];
+                    if (Subsequence.Length > Sequence.Length)
+                    {
+                        Sequence = Subsequence;
+                    }
+                }
+                else
+                {
+                    //Step back to set repeting symbol Line[i] into new subsequence
+                    i--;
+                    Subsequence = String.Empty;
+                }
+
+            }
+            Console.WriteLine("The longest sequence of unique symbols is: \"" + Sequence + "\"");
+
+        }
+        else
+        {
+            Console.WriteLine("String is null or emty");
+        }
+
+        Console.ReadKey();
+    }
+
+}
