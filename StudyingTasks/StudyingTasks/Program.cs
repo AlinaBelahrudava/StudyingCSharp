3b55e47dc9fb84f68f3c0d490d96dd305212f3b4
AlinaBelahrudava <alina.belahrudava@gmail.com>
2022-09-15 17:12:30 +0300

    add tests

--- StudyingTasks/StudyingTasks/Program.cs
+++ StudyingTasks/StudyingTasks/Program.cs
@@ -1,43 +1,23 @@
-﻿using System;
+﻿using StudyingTasks.StringAnalysys;
 
-class Program
+
+public class Program
 {
 
-    static void Main(string[] args)
+    public static void Main()
     {
 
         Console.WriteLine("Please enter your string and I will find the longest sequence of unique ones");
-        string? Line = Console.ReadLine();
-        string Sequence = String.Empty;
+        string? line = Console.ReadLine();
+        StringAnalyzer stringAnalizer = new();
+        stringAnalizer.GetMaxUniqueSequence(line);
+        Console.Write("Max sequence of the same letters: ");
+        Console.WriteLine(stringAnalizer.GetMaxSameLettersSequence(line));
+        Console.Write("Max sequence of the same digits: ");
+        Console.WriteLine(stringAnalizer.GetMaxSameDigitsSequence(line));
 
-        if (!String.IsNullOrEmpty(Line))
-        {
-            string Subsequence = String.Empty;
-            for (int i = 0; i < Line.Length; i++)
-            {
-                if (!Subsequence.Contains(Line[i]))
-                {
-                    Subsequence += Line[i];
-                    if (Subsequence.Length > Sequence.Length)
-                    {
-                        Sequence = Subsequence;
-                    }
-                }
-                else
-                {
-                    //Step back to set repeting symbol Line[i] into new subsequence
-                    i--;
-                    Subsequence = String.Empty;
-                }
 
-            }
-            Console.WriteLine("The longest sequence of unique symbols is: \"" + Sequence + "\"");
 
-        }
-        else
-        {
-            Console.WriteLine("String is null or emty");
-        }
 
         Console.ReadKey();
     }
